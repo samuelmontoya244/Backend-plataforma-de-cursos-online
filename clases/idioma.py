9ff9b8a53fd584d9b90fc3ff77433bcfc5077eb6
pekkamaster <aejfbsejfffh@gmail.com>
2026-02-23 19:06:26 -0500

    feat: modifique algunos atributos, agregue su respectivo set y get, una validacion de idioma con el nombre y agregue un str

--- clases/idioma.py
+++ clases/idioma.py
@@ -1,10 +1,46 @@
 class idioma:
-    def __init__(self, idioma:str, nivel:str, dificultad:int, estado:str, id:int):
-        self.nivel = nivel
-        self.dificultad = dificultad
-        self.estado = estado
-        self.idioma = idioma
-        self.id = id
+    def __init__(self, nombre: str, nivel: str, dificultad: str, horas: int, id: int):
+        self._nombre = nombre
+        self._nivel = nivel
+        self._dificultad = dificultad
+        self._horas = horas
+        self._id = id
+
+    def get_nombre(self) -> str:
+        return self._nombre
+
+    def get_nivel(self) -> str:
+        return self._nivel
+
+    def get_dificultad(self) -> str:
+        return self._dificultad
+
+    def get_horas(self) -> int:
+        return self._horas
+
+    def get_id(self) -> int:
+        return self._id
+
+    def set_nombre(self, nombre) -> None:
+        self._nombre = nombre
+
+    def set_nivel(self, nivel) -> None:
+        self._nivel = nivel
+
+    def set_dificultad(self, dificultad) -> None:
+        self._dificultad = dificultad
+
+    def set_horas(self, horas) -> None:
+        self._horas = horas
+
+    def set_id(self, id) -> None:
+        self._id = id
+
+    def validar_nombre(self, nombre) -> bool:
+        if nombre in ["python", "SQL", "java", "java_script", "c++", "html"]:
+            return True
+        else:
+            return False
+
     def __str__(self):
-        return f"Idioma: {self.idioma}, Nivel: {self.nivel}, Dificultad: {self.dificultad}, Estado: {self.estado}"
-    
+        return f"Nombre: {self._nombre}, Nivel: {self._nivel}, Dificultad: {self._dificultad}, Horas: {self._horas}, Id: {self._id}"
