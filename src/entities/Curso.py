0191de36c423a5ab05a6b3c3c3a8f1b95e5128c0
pekkamaster <aejfbsejfffh@gmail.com>
2026-02-25 16:15:42 -0500

    feat: cree dos clases y elimine una

---
+++ src/entities/Curso.py
@@ -0,0 +1,52 @@
+from instructor import Instructor
+from src.entities import instructor
+
+
+class Curso:
+
+    def __init__(
+        self,
+        codigo: int = None,
+        nombre: str = None,
+        categoria: str = None,
+        duracionhoras: int = None,
+        instructor: Instructor = None,
+    ):
+        self.__codigo = codigo
+        self.__nombre = nombre
+        self.__categoria = categoria
+        self.__duracionhoras = duracionhoras
+        self.__instructor = instructor
+
+    def get_codigo(self) -> int:
+        return self.__codigo
+
+    def get_nombre(self) -> str:
+        return self.__nombre
+
+    def get_categoria(self) -> str:
+        return self.__categoria
+
+    def get_duracionhoras(self) -> int:
+        return self.__duracionhoras
+
+    def get_instructor(self) -> Instructor:
+        return self.__instructor
+
+    def set_codigo(self, codigo: int) -> None:
+        self.__codigo = codigo
+
+    def set_nombre(self, nombre: str) -> None:
+        self.__nombre = nombre
+
+    def set_categoria(self, categoria: str) -> None:
+        self.__categoria = categoria
+
+    def set_duracionhoras(self, duracionhoras: int) -> None:
+        self.__duracionhoras = duracionhoras
+
+    def set_instructor(self, instructor: Instructor) -> None:
+        self.__instructor = instructor
+
+    def __str__(self) -> str:
+        return f"{self.__codigo}, {self.__nombre}, {self.__categoria}, {self.__duracionhoras}, {self.__instructor}"
---
+++ src/entities/Inscripciones.py
@@ -0,0 +1,52 @@
+from estudiante import Estudiante
+from Curso import Curso
+from datetime import date
+
+
+class Inscripciones:
+    def __init__(
+        self,
+        fecha_inscripcion: date = None,
+        nota_final: float = None,
+        estado: bool = None,
+        estudiante: Estudiante = None,
+        curso: Curso = None,
+    ):
+        self.__fecha_inscripcion = fecha_inscripcion
+        self.__nota_final = nota_final
+        self.__estado = estado
+        self.__estudiante = estudiante
+        self.__curso = curso
+
+    def get_fecha_inscripcion(self) -> date:
+        return self.__fecha_inscripcion
+
+    def get_nota_final(self) -> float:
+        return self.__nota_final
+
+    def get_estado(self) -> bool:
+        return self.__estado
+
+    def get_estudiante(self) -> Estudiante:
+        return self.__estudiante
+
+    def get_curso(self) -> Curso:
+        return self.__curso
+
+    def set_fecha_inscripcion(self, fecha_inscripcion: date) -> None:
+        self.__fecha_inscripcion = fecha_inscripcion
+
+    def set_nota_final(self, nota_final: float) -> None:
+        self.__nota_final = nota_final
+
+    def set_estado(self, estado: bool) -> None:
+        self.__estado = estado
+
+    def set_estudiante(self, estudiante: Estudiante) -> None:
+        self.__estudiante = estudiante
+
+    def set_curso(self, curso: Curso) -> None:
+        self.__curso = curso
+
+    def __str__(self) -> str:
+        return f"{self.__fecha_inscripcion}, {self.__nota_final}, {self.__estado}, {self.__estudiante}, {self.__curso}"
