50dcc9e2f2d64b87a3041d0f8f01e0c15ac21ce5
samuelmontoya244 <samuelmontoya1135943@correo.itm.edu.co>
2026-02-25 22:42:48 -0500

    feat: agregar metodos a plataforma e inscripciones

--- src/entities/plataforma.py
+++ src/entities/plataforma.py
@@ -1,6 +1,7 @@
 from src.entities.estudiante import Estudiante
 from src.entities.curso import Curso
 from src.entities.instructor import Instructor
+from src.entities.inscripciones import Inscripciones
 
 
 class Plataforma:
@@ -9,6 +10,7 @@ class Plataforma:
         self.__estudiantes = []
         self.__cursos = []
         self.__instructores = []
+        self.__inscripciones = []
 
     def crear_estudiante(
         self,
@@ -17,7 +19,12 @@ class Plataforma:
         apellido: str,
         cedula: int,
         telefono: int,
-    ) -> None:
+    ) -> Estudiante:
+        
+        for est in self.__estudiantes:
+            if est.cedula == cedula:
+                raise ValueError(f"Ya existe un estudiante con la cédula {cedula}")
+            
         nuevo_estudiante = Estudiante(id_usuario, nombre, apellido, cedula, telefono)
         self.__estudiantes.append(nuevo_estudiante)
         return nuevo_estudiante
@@ -48,3 +55,36 @@ class Plataforma:
         nuevo_curso = Curso(codigo, nombre, categoria, duracionhoras, instructor)
         self.__cursos.append(nuevo_curso)
         return nuevo_curso
+    
+    def obtener_estudiante_por_id(self, id_usuario):
+        for e in self.__estudiantes:
+            if e.get_id_usuario() == id_usuario:
+                return e
+        return None
+    
+    def obtener_curso_por_codigo(self, codigo):
+        for c in self.__cursos:
+            if c.get_codigo() == codigo:
+                return c
+        return None
+    
+    def obtener_instructor_por_id(self, id_usuario):
+        for i in self.__instructores:
+            if i.get_id_usuario() == id_usuario:
+                return i
+        return None
+
+    def inscribir_estudiante_a_curso(self, estudiante: Estudiante, curso: Curso) -> None:
+        if estudiante in self.__estudiantes and curso in self.__cursos:
+            nueva_inscripcion = Inscripciones(estudiante=estudiante, curso=curso)
+            self.__inscripciones.append(nueva_inscripcion)
+            print(f"Éxito: {estudiante.get_nombre()} inscrito en {curso.get_nombre()}.")
+        else:
+            raise ValueError("El estudiante o el curso no existen en la plataforma.")
+        
+    def calificar_estudiante(self, estudiante, curso, nota_final):
+        for inscripcion in self.__inscripciones:
+            if inscripcion.get_estudiante() == estudiante and inscripcion.get_curso() == curso:
+                inscripcion.registrar_calificacion(nota_final)
+                return True
+        return False
