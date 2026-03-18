4572da9f33dacba06c0f5c2cfc39cf24464c8b9c
pekkamaster <aejfbsejfffh@gmail.com>
2026-03-17 21:08:27 -0500

    feat:cree las identidades, pago, materiales, leccion y evaluacion, y corregi algunos errores

---
+++ src/entities/Material.py
@@ -0,0 +1,40 @@
+import uuid
+
+from sqlalchemy import Column, ForeignKey, String
+from sqlalchemy.dialects.postgresql import UUID
+from sqlalchemy.orm import relationship
+
+from src.database.config import Base
+
+class Material(Base):
+    """Modelo de material"""
+
+    __tablename__ = "material"
+
+    id_material = Column(
+        UUID(as_uuid=True),primary_key=True,default=uuid.uuid4,index=True,
+        
+    )
+    
+    id_leccion = Column(
+        UUID(as_uuid=True), ForeignKey("leccion.id_leccion"), nullable=False
+    )
+
+    titulo_material = Column[str](String(20),nullable=False)
+    tipo_material = Column[str](String(20), nullable=True)
+    URL_archivo = Column[str](String(255), nullable=False)
+
+    
+    id_usuario_creacion = Column(
+        UUID(as_uuid=True), ForeignKey("usuario.id_usuario"), nullable=False
+    )
+    id_usuario_edita = Column(
+        UUID(as_uuid=True), ForeignKey("usuario.id_usuario"), nullable=True
+    )
+
+    usuario_creacion = relationship("Usuario", foreign_keys=[id_usuario_creacion])
+    usuario_edita = relationship("Usuario", foreign_keys=[id_usuario_edita])
+
+
+
+    leccion = relationship("Leccion", foreign_keys=[id_leccion])
