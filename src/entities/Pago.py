69467fb358b7d8a3ddea840794421fa26d831663
pekkamaster <aejfbsejfffh@gmail.com>
2026-03-17 21:12:53 -0500

    feat:cree las identidades, pago, materiales, leccion y evaluacion, y corregi algunos errores

---
+++ src/entities/Pago.py
@@ -0,0 +1,45 @@
+import uuid
+
+from sqlalchemy import Column, ForeignKey, String, Float, DateTime, func
+from sqlalchemy.dialects.postgresql import UUID
+from sqlalchemy.orm import relationship
+
+from src.database.config import Base
+
+
+class Pago(Base):
+    """Modelo de pago"""
+
+    __tablename__ = "pago"
+
+    id_pago = Column(
+        UUID(as_uuid=True),primary_key=True,default=uuid.uuid4,index=True,
+        
+    )
+    id_usuario = Column(
+        UUID(as_uuid=True), ForeignKey("leccion.id_leccion"), nullable=False
+    )
+
+    id_curso = Column(
+        UUID(as_uuid=True), ForeignKey("curso.id_curso"), nullable=False
+    )
+
+
+    monto = Column[float](Float, nullable=False)
+    estado_pago = Column[str](String(20), nullable=True)
+    metodo_pago = Column[str](String(50), nullable=False)
+
+    fecha_pago = Column(DateTime(timezone=True), server_default=func.now())
+
+    id_usuario_creacion = Column(
+        UUID(as_uuid=True), ForeignKey("usuario.id_usuario"), nullable=False
+    )
+    
+    id_usuario_edita = Column(
+        UUID(as_uuid=True), ForeignKey("usuario.id_usuario"), nullable=True
+    )
+
+    usuario_creacion = relationship("Usuario", foreign_keys=[id_usuario_creacion])
+    usuario_edita = relationship("Usuario", foreign_keys=[id_usuario_edita])
+
+    curso = relationship("Curso", foreign_keys=[id_curso])
