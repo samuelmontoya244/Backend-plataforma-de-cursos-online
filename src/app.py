afd99545ca47a7af12b8e01fbf8e065eb488770e
samuelmontoya244 <samuelmontoya1135943@correo.itm.edu.co>
2026-04-07 11:31:26 -0500

    feat:ajustes a endpoints y services

--- src/app.py
+++ src/app.py
@@ -9,14 +9,8 @@ from fastapi import FastAPI
 from src.database.config import create_tables
 from src.endpoints import (
     usuario,
-<<<<<<< Updated upstream
-    leccion,
-    material,
-    pago
-=======
     #inscripcion,
     #curso
->>>>>>> Stashed changes
 )
 
 @asynccontextmanager
@@ -33,15 +27,8 @@ app = FastAPI(
 )
 
 app.include_router(usuario.router)
-<<<<<<< Updated upstream
-#app.include_router(leccion.router)
-#app.include_router(material.router)
-#app.include_router(pago.router)
-=======
 #app.include_router(inscripcion.router)
 #app.include_router(curso.router)
-
->>>>>>> Stashed changes
 
 @app.get("/")
 def inicio():
